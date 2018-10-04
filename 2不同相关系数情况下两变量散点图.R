fdb0f9da7fc7a0e891f7eac5bc5c946aa382dcf0
fteng77 <fteng77@126.com>
2018-10-04 09:58:07 +0800

    update

--- 2不同相关系数情况下两变量散点图.R
+++
@@ -1,8 +0,0 @@
-library(mvtnorm)
-#library(ggplot2)
-rho<-0.5
-sigma<-matrix(c(1,rho,rho,1),nrow=2)
-xx<-rmvnorm(1000,mean=rep(0,nrow(sigma)),sigma=sigma)
-main<-paste("相关系数为",rho,sep="")
-plot(xx,xlab="",ylab="",main=main)
-
